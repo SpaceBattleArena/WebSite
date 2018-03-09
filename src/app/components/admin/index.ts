7bc83c2a372460de7a756c59d0463ec39dc88145
AdrienMS <adrien.musserotte@gmail.com>
2018-03-10 00:30:38 +0100

    delete old ts files

--- src/app/components/admin/index.ts
+++ src/app/components/admin/index.ts
@@ -1,6 +1,3 @@
 export * from './admin.component';
-export * from './components/news-list.component';
-export * from './components/players_list.component';
 export * from './components/dashboard.component';
-export * from './components/create_news.component';
 export * from './components/items.component';
