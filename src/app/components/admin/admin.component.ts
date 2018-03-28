0e0a417b9101536b7b605b6300785b2fe98fe5d3
fueledbysushis <fueledbysushis@gmail.com>
2018-03-28 16:05:32 +0200

    test

--- src/app/components/admin/admin.component.ts
+++ src/app/components/admin/admin.component.ts
@@ -21,23 +21,23 @@ export class AdminComponent implements OnInit {
     }
 
     ngOnInit() {
-        $('body').addClass("fixed-sn black-skin");
+        $('body').addClass('fixed-sn black-skin');
         $('body').css('background-image', 'none');
         $('body').css('background-color', '#dadada');
         $('#ships').css('display', 'none');
         $('.small-nav-bttom').css('background-color', '#000614');
         this.currentUser = JSON.parse(localStorage.getItem('currentUser'));
         if (this.currentUser) {
-            this.userService.getInformation(this.currentUser["token"])
+            this.userService.getInformation(this.currentUser['token'])
                 .subscribe(
                     resultArray => {
                         console.log(resultArray);
-                        this.is_staff = resultArray["results"]["data"][0]["Is_staff"];
-                        if(!this.is_staff) {
+                        this.is_staff = resultArray['results']['data'][0]['Is_staff'];
+                        if (!this.is_staff) {
                             this.router.navigate(['/login']);
                         }
                     }
-                )
+                );
         }
     }
 }
